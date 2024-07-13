0880ff03a8561f813cce9913caa9e3cd2b079378
dongmingyan <dongmingyan01@gmail.com>
2024-07-13 15:34:52 +0800

    rpc model 关联 && 注册完成

--- service/user/rpc/internal/logic/registerlogic.go
+++ service/user/rpc/internal/logic/registerlogic.go
@@ -2,13 +2,13 @@ package logic
 
 import (
 	"context"
-	"errors"
 
 	"forum/service/user/model"
 	"forum/service/user/rpc/internal/svc"
 	"forum/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type RegisterLogic struct {
@@ -27,22 +27,38 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 
 func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterResponse, error) {
 	// 注册时 先去数据库查找下是否已经有这个人
-	u, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
-	if err != nil {
-		return &user.RegisterResponse{}, err
+	_, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
+	if err == nil {
+		return &user.RegisterResponse{}, status.Error(400, "该手机号已注册")
 	}
 
-	if u != nil {
-		return &user.RegisterResponse{}, errors.New("该手机号已注册")
+	// 加这个判断是为了避免其它错误导致去创建
+	if err == model.ErrNotFound {
+		res, err := l.svcCtx.UserModel.Insert(l.ctx,
+			&model.Users{
+				Name:     in.Name,
+				Mobile:   in.Mobile,
+				Password: in.Password,
+				Gender:   in.Gender,
+			})
+
+		if err != nil {
+			return &user.RegisterResponse{}, status.Error(400, err.Error())
+		}
+
+		id, err := res.LastInsertId()
+		if err != nil {
+			return &user.RegisterResponse{}, status.Error(400, err.Error())
+		}
+
+		return &user.RegisterResponse{
+			Id:     id,
+			Name:   in.Name,
+			Gender: in.Gender,
+			Mobile: in.Mobile,
+		}, nil
 	}
 
-	l.svcCtx.UserModel.Insert(l.ctx,
-		&model.Users{
-			Name:     in.Name,
-			Mobile:   in.Mobile,
-			Password: in.Password,
-			Gender:   in.Gender,
-		})
-
-	return &user.RegisterResponse{}, nil
+	// 如果不是ErrNotFound，则说明数据库查询出错,这里报错500
+	return &user.RegisterResponse{}, status.Error(500, err.Error())
 }
