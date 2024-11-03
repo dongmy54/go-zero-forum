f33a4d497f5ae3a80af1b57073d2774fcf5d7de9
dongmingyan <dongmingyan01@gmail.com>
2024-11-03 18:39:41 +0800

    add comment show api

--- service/comment/cmd/api/internal/logic/showCommentLogic.go
+++ service/comment/cmd/api/internal/logic/showCommentLogic.go
@@ -5,6 +5,7 @@ import (
 
 	"forum/service/comment/cmd/api/internal/svc"
 	"forum/service/comment/cmd/api/internal/types"
+	"forum/service/comment/cmd/rpc/pb"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -26,5 +27,18 @@ func NewShowCommentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShowC
 
 func (l *ShowCommentLogic) ShowComment(req *types.ShowCommentReq) (resp *types.ShowCommentResp, err error) {
 	// todo: add your logic here and delete this line
-	return
+	res, err := l.svcCtx.CommentRpc.ShowComment(l.ctx, &pb.ShowCommentReq{
+		Id: req.Id,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	l.Debugf("======show comment info:%#v===", res)
+
+	return &types.ShowCommentResp{
+		UserId: res.UserId,
+		Desc:   res.Desc,
+	}, nil
 }
