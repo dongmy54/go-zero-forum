5de9a236789454f91f959c4c32ee17a3dc7b1c53
dongmingyan <dongmingyan01@gmail.com>
2024-11-16 22:48:59 +0800

    update

--- common/middelware/authMiddelWare.go
+++ common/middelware/authMiddelWare.go
@@ -1,9 +1,11 @@
 package middelware
 
 import (
+	"forum/service/comment/cmd/rpc/comment"
 	"net/http"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest"
 )
 
 // AuthMiddleware 是一个简单的认证中间件
@@ -19,5 +21,23 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 		// 认证通过，调用下一个处理器
 		next(w, r)
+	}
+}
+
+func NewVaildateMiddleware(commentRpc comment.Comment) rest.Middleware {
+	// 嵌套多层
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			logx.Debug("===========验证数据的middelware======")
+			res, err := commentRpc.ShowComment(r.Context(), &comment.ShowCommentReq{
+				Id: 18})
+			if err != nil {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			}
+
+			logx.Debug("===========验证数据的调用数据为：======", res)
+			// 认证通过，调用下一个处理器
+			next(w, r)
+		}
 	}
 }
--- service/comment/cmd/api/comment.go
+++ service/comment/cmd/api/comment.go
@@ -8,9 +8,11 @@ import (
 	"forum/service/comment/cmd/api/internal/config"
 	"forum/service/comment/cmd/api/internal/handler"
 	"forum/service/comment/cmd/api/internal/svc"
+	"forum/service/comment/cmd/rpc/comment"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 var configFile = flag.String("f", "etc/comment.yaml", "the config file")
@@ -26,6 +28,12 @@ func main() {
 	// 添加api全局中间件 还可以对单个路由添加中间件
 	server.Use(middelware.LoggingMiddleware)
 	server.Use(middelware.AuthMiddleware)
+
+	// 初始化RPC客户端
+	commentRpcClient := comment.NewComment(zrpc.MustNewClient(c.CommentRpcConf))
+	// 注入一个验证的middleware
+	validaMiddleWare := middelware.NewVaildateMiddleware(commentRpcClient)
+	server.Use(validaMiddleWare)
 
 	defer server.Stop()
 
